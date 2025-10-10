004eeb43ab0fa6fc5ea0b1335c0f1eab86442378
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-10-10 11:09:06 +0600

    feat: implement updateProfile method in user controller for profile updates

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { userService } from "./user.service";
+import sendResponse from "../../utils/sendResponse";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const updateProfile = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+  const { bio } = req.body;
+  const { id } = req.user;
+  const result = await userService.updateProfile({ bio }, id);
+  sendResponse(res, {
+    success: true,
+    message: "User profile updated successfully",
+    data: result,
+    statusCode: 200,
+  });
+});
+
+export const userController = {
+  updateProfile,
+};

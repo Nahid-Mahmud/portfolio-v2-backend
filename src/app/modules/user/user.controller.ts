f5db716de43b2e4b48abd13fd9b58feca0525570
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-10-11 19:02:20 +0600

    feat: enhance user profile update to include photo upload handling

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,12 +2,19 @@ import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { userService } from "./user.service";
 import sendResponse from "../../utils/sendResponse";
+import { handleSingleFileUpload } from "../../utils/handleFileUpload";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const updateProfile = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
-  const payload = req.body;
   const { id } = req.user;
-  const result = await userService.updateProfile(payload, id);
+  const payload = req.body;
+  let photo = "";
+
+  if (req.file) {
+    photo = await handleSingleFileUpload(req.file, "profile");
+  }
+
+  const result = await userService.updateProfile({ ...payload, photo }, id);
   sendResponse(res, {
     success: true,
     message: "User profile updated successfully",

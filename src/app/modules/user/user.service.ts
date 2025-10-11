7faab04d3ae0e71d0f346c1c8b6adaf6cf3981d3
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-10-11 19:02:24 +0600

    feat: update profile service to handle photo deletion and improve payload structure

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,11 +1,18 @@
 import { User } from "@prisma/client";
 import { prisma } from "../../../config/db";
+import { deleteFileFormCloudinary } from "../../../config/cloudinary.config";
 
-const updateProfile = async (payload: Partial<User>, id: string) => {
+const updateProfile = async (payload: Partial<User> & { deletePhoto?: string }, id: string) => {
+  const { deletePhoto, ...rest } = payload;
   const result = await prisma.user.update({
     where: { id },
-    data: payload,
+    data: rest,
   });
+
+  if (deletePhoto) {
+    await deleteFileFormCloudinary(deletePhoto);
+  }
+
   return result;
 };
 
